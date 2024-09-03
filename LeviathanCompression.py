256af352e2945b6c07cfc61655d86860bac3cfab
Antony Naumovic <40827853+antonynaumovic@users.noreply.github.com>
2024-09-03 18:50:49 +0100

    CV2

--- LeviathanCompression.py
+++ LeviathanCompression.py
@@ -9,13 +9,20 @@ from joblib import Parallel, delayed
 
 open_file = filedialog.askdirectory()
 
+max_size = 4096
+
 
 def joblib_loop(i):
     file_path = images[i]
     try:
         original_image = cv2.imread(file_path)
-        cv2.imwrite(file_path, original_image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
-        print("Compressed: " + os.path.basename(file_path))
+        original_size = round(os.stat(file_path).st_size/1024, 2)
+        width, height, channels = original_image.shape
+        if width > max_size:
+            original_image = cv2.resize(original_image, (max_size, max_size), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(file_path, original_image, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
+        new_size = round(os.stat(file_path).st_size/1024, 2)
+        print(f"Compressed: {os.path.basename(file_path)} From {original_size} to {new_size}")
     except Exception as e:
         print(e)
 
